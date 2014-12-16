eec40059979ec65c380dfc6e7eb2ab1df478858f
Peter Zotov <whitequark@whitequark.org>
2014-12-16 05:22:41 +0300

    -safe-string compatibility.

--- lib/base64.ml
+++ lib/base64.ml
@@ -26,34 +26,40 @@ let of_char ?(alphabet=default_alphabet) x =
 let to_char ?(alphabet=default_alphabet) x =
   alphabet.[x]
 
-let decode ?alphabet x =
-  let words = String.length x / 4 in
+let decode ?alphabet input =
+  let length = String.length input in
+  let words = length / 4 in
   let padding =
-    if String.length x = 0 then 0 else (
-    if x.[String.length x - 2] = padding
-    then 2 else (if x.[String.length x - 1] = padding then 1 else 0)) in
-  let output = String.make (words * 3 - padding) '\000' in
+    match length with
+    | 0 -> 0
+    | _ when input.[length - 2] = padding -> 2
+    | _ when input.[length - 1] = padding -> 1
+    | _ -> 0
+  in
+  let output = Bytes.make (words * 3 - padding) '\000' in
   for i = 0 to words - 1 do
-    let a = of_char ?alphabet x.[4 * i + 0]
-    and b = of_char ?alphabet x.[4 * i + 1]
-    and c = of_char ?alphabet x.[4 * i + 2]
-    and d = of_char ?alphabet x.[4 * i + 3] in
+    let a = of_char ?alphabet (String.get input (4 * i + 0))
+    and b = of_char ?alphabet (String.get input (4 * i + 1))
+    and c = of_char ?alphabet (String.get input (4 * i + 2))
+    and d = of_char ?alphabet (String.get input (4 * i + 3)) in
     let n = (a lsl 18) lor (b lsl 12) lor (c lsl 6) lor d in
     let x = (n lsr 16) land 255
     and y = (n lsr 8) land 255
     and z = n land 255 in
-    output.[3 * i + 0] <- char_of_int x;
-    if i <> words - 1 || padding < 2 then output.[3 * i + 1] <- char_of_int y;
-    if i <> words - 1 || padding < 1 then output.[3 * i + 2] <- char_of_int z;
+    Bytes.set output (3 * i + 0) (char_of_int x);
+    if i <> words - 1 || padding < 2 then
+      Bytes.set output (3 * i + 1) (char_of_int y);
+    if i <> words - 1 || padding < 1 then
+      Bytes.set output (3 * i + 2) (char_of_int z);
   done;
-  output
+  Bytes.unsafe_to_string output
 
-let encode ?alphabet x =
-  let length = String.length x in
+let encode ?alphabet input =
+  let length = String.length input in
   let words = (length + 2) / 3 in (* rounded up *)
   let padding_len = if length mod 3 = 0 then 0 else 3 - (length mod 3) in
-  let output = String.make (words * 4) '\000' in
-  let get i = if i >= length then 0 else int_of_char x.[i] in
+  let output = Bytes.make (words * 4) '\000' in
+  let get i = if i >= length then 0 else int_of_char input.[i] in
   for i = 0 to words - 1 do
     let x = get (3 * i + 0)
     and y = get (3 * i + 1)
@@ -63,12 +69,12 @@ let encode ?alphabet x =
     and b = (n lsr 12) land 63
     and c = (n lsr 6) land 63
     and d = n land 63 in
-    output.[4 * i + 0] <- to_char ?alphabet a;
-    output.[4 * i + 1] <- to_char ?alphabet b;
-    output.[4 * i + 2] <- to_char ?alphabet c;
-    output.[4 * i + 3] <- to_char ?alphabet d;
+    Bytes.set output (4 * i + 0) (to_char ?alphabet a);
+    Bytes.set output (4 * i + 1) (to_char ?alphabet b);
+    Bytes.set output (4 * i + 2) (to_char ?alphabet c);
+    Bytes.set output (4 * i + 3) (to_char ?alphabet d);
   done;
   for i = 1 to padding_len do
-    output.[String.length output - i] <- padding;
+    Bytes.set output (Bytes.length output - i) padding;
   done;
-  output
+  Bytes.unsafe_to_string output
