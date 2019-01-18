6693cf5e9a5cb03095ec6fb4508bfbbb47df9480
Calascibetta Romain <romain.calascibetta@gmail.com>
2019-01-18 19:15:11 +0100

    Randomize on fuzzer RFC 4648 off and len arguments

--- fuzz/fuzz_rfc4648.ml
+++ fuzz/fuzz_rfc4648.ml
@@ -45,26 +45,32 @@ let random_string_from_alphabet alphabet len : string gen =
   add_char_from_alphabet (const "") len
 
 let random_string_from_alphabet ~max alphabet =
-  dynamic_bind (range max) (random_string_from_alphabet alphabet)
+  dynamic_bind (range max)
+  @@ fun real_len ->
+  dynamic_bind (random_string_from_alphabet alphabet real_len)
+  @@ fun input ->
+  if real_len <= 1 then const (input, 0, real_len)
+  else dynamic_bind (range (real_len / 2))
+    @@ fun off -> map [ range (real_len - off) ] (fun len -> (input, off, len))
 
-let encode_and_decode input =
-  match B64.encode ~pad:true input with
+let encode_and_decode (input, off, len) =
+  match B64.encode ~pad:true ~off ~len input with
   | Error (`Msg err) -> fail err
   | Ok result ->
       match B64.decode ~pad:true result with
       | Error (`Msg err) -> fail err
       | Ok result ->
-          check_eq ~pp ~cmp:String.compare ~eq:String.equal result input
+          check_eq ~pp ~cmp:String.compare ~eq:String.equal result (String.sub input off len)
 
-let decode_and_encode input =
-  match B64.decode ~pad:true input with
+let decode_and_encode (input, off, len) =
+  match B64.decode ~pad:true ~off ~len input with
   | Error (`Msg err) ->
       fail err
   | Ok result ->
       match B64.encode ~pad:true result with
       | Error (`Msg err) -> fail err
       | Ok result ->
-          check_eq ~pp:Fmt.string ~cmp:String.compare ~eq:String.equal result input
+          check_eq ~pp:Fmt.string ~cmp:String.compare ~eq:String.equal result (String.sub input off len)
 
 let (//) x y =
   if y < 1 then raise Division_by_zero ;
@@ -74,16 +80,21 @@ let (//) x y =
 let canonic alphabet =
   let dmap = Array.make 256 (-1) in
   Array.iteri (fun i x -> Array.set dmap x i) (B64.alphabet alphabet) ;
-  fun input ->
-    let input_len = String.length input in
+  fun (input, off, len) ->
+    let real_len = String.length input in
+    let input_len = len in
     let normalized_len = (input_len // 4) * 4 in
-    if normalized_len = input_len then input
-    else if normalized_len - input_len = 3 then String.sub input 0 (input_len - 1)
+    if normalized_len = input_len then (input, off, input_len)
+    else if normalized_len - input_len = 3 then (input, off, input_len - 1)
     else begin
       let remainder_len = normalized_len - input_len in
-      let last = String.get input (input_len - 1) in
-      let output = Bytes.make normalized_len '=' in
-      Bytes.blit_string input 0 output 0 input_len ;
+      let last = String.get input (off + input_len - 1) in
+      let output = Bytes.make (max real_len (off + normalized_len)) '=' in
+
+      Bytes.blit_string input 0 output 0 (off + input_len);
+      if off + normalized_len < real_len
+      then Bytes.blit_string input (off + normalized_len) output (off + normalized_len) (real_len - (off + normalized_len)) ;
+
       let mask = match remainder_len with
         | 1 -> 0x3c
         | 2 -> 0x30
@@ -91,13 +102,13 @@ let canonic alphabet =
       let decoded = Array.get dmap (Char.code last) in
       let canonic = (decoded land mask) in
       let encoded = Array.get (B64.alphabet alphabet) canonic in
-      Bytes.set output (input_len - 1) (Char.chr encoded) ;
-      Bytes.unsafe_to_string output
+      Bytes.set output (off + input_len - 1) (Char.chr encoded) ;
+      (Bytes.unsafe_to_string output, off, normalized_len)
     end
 
-let isomorphism0 input =
+let isomorphism0 (input, off, len) =
   (* x0 = decode(input) && x1 = decode(encode(x0)) && x0 = x1 *)
-  match B64.decode ~pad:false input with
+  match B64.decode ~pad:false ~off ~len input with
   | Error (`Msg err) ->
       fail err
   | Ok result0 ->
@@ -108,16 +119,30 @@ let isomorphism0 input =
       | Ok result2 ->
           check_eq ~pp ~cmp:String.compare ~eq:String.equal result0 result2
 
-let isomorphism1 input =
-  let result0 = B64.encode_exn input in
+let isomorphism1 (input, off, len) =
+  let result0 = B64.encode_exn ~off ~len input in
   match B64.decode ~pad:true result0 with
   | Error (`Msg err) -> fail err
   | Ok result1 ->
       let result2 = B64.encode_exn result1 in
       check_eq ~pp:Fmt.string ~cmp:String.compare ~eq:String.equal result0 result2
 
+let bytes_and_range : (string * int * int) gen =
+  dynamic_bind bytes
+  @@ fun t ->
+  let real_length = String.length t in
+  if real_length <= 1
+  then const (t, 0, real_length)
+  else dynamic_bind (range (real_length / 2))
+    @@ fun off ->
+    map [ range (real_length - off) ] (fun len -> (t, off, len))
+
+let range_of_max max : (int * int) gen =
+  dynamic_bind (range (max / 2))
+  @@ fun off -> map [ range (max - off) ] (fun len -> (off, len))
+
 let () =
-  add_test ~name:"rfc4648: encode -> decode" [ bytes ] encode_and_decode ;
+  add_test ~name:"rfc4648: encode -> decode" [ bytes_and_range ] encode_and_decode ;
   add_test ~name:"rfc4648: decode -> encode" [ random_string_from_alphabet ~max:1000 B64.default_alphabet ] (decode_and_encode <.> canonic B64.default_alphabet) ;
   add_test ~name:"rfc4648: x = decode(encode(x))" [ random_string_from_alphabet ~max:1000 B64.default_alphabet ] isomorphism0 ;
-  add_test ~name:"rfc4648: x = encode(decode(x))" [ bytes ] isomorphism1
+  add_test ~name:"rfc4648: x = encode(decode(x))" [ bytes_and_range ] isomorphism1
