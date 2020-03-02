48d46c88d4c75a8cc7ca79e5cb6d14f437f97928
Calascibetta Romain <romain.calascibetta@gmail.com>
2020-03-02 12:47:03 +0100

    Add benchmarks to compare with mirage-crypto

--- bench/benchmarks.ml
+++ bench/benchmarks.ml
@@ -1,3 +1,93 @@
+module Crypto_version = struct
+  open Cstruct
+  
+  let (//) x y =
+    if y < 1 then raise Division_by_zero else
+      if x > 0 then 1 + ((x - 1) / y) else 0 [@@inline]
+  
+  let sym     = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+  let padding = int_of_char '='
+  
+  let (emap, dmap) =
+    let make_ht f =
+      let ht = Hashtbl.create 64 in
+      for i = 0 to String.length sym - 1 do
+        f ht i (int_of_char sym.[i])
+      done ;
+      Hashtbl.find ht in
+    (make_ht Hashtbl.add),
+    (make_ht (fun ht i c -> Hashtbl.add ht c i))
+  
+  let encode cs =
+  
+    let n   = len cs in
+    let n'  = n // 3 * 4 in
+    let cs' = create n' in
+  
+    let emit b1 b2 b3 i =
+      BE.set_uint16 cs' i
+        ((emap (b1 lsr 2 land 0x3f) lsl 8) lor
+        (emap ((b1 lsl 4) lor (b2 lsr 4) land 0x3f))) ;
+      BE.set_uint16 cs' (i + 2)
+        ((emap ((b2 lsl 2) lor (b3 lsr 6) land 0x3f) lsl 8) lor
+        (emap (b3 land 0x3f))) in
+  
+    let rec enc j = function
+      | i when i = n     -> ()
+      | i when i = n - 1 ->
+          emit (get_uint8 cs i) 0 0 j
+      | i when i = n - 2 ->
+          emit (get_uint8 cs i) (get_uint8 cs (i + 1)) 0 j
+      | i ->
+          emit (get_uint8 cs i) (get_uint8 cs (i + 1)) (get_uint8 cs (i + 2)) j ;
+          enc (j + 4) (i + 3)
+  
+    and fix = function
+      | 0 -> ()
+      | i -> set_uint8 cs' (n' - i) padding ; fix (i - 1) in
+  
+    enc 0 0 ;
+    fix ((3 - n mod 3) mod 3) ;
+    cs'
+  
+  let decode cs =
+  
+    let n   = len cs in
+    let n'  = (n / 4) * 3 in
+    let cs' = create n' in
+  
+    let emit a b c d i =
+      let x = (a lsl 18) lor (b lsl 12) lor (c lsl 6) lor d in
+      BE.set_uint16 cs' i (x lsr 8) ;
+      set_uint8 cs' (i + 2) (x land 0xff) in
+  
+    let rec dec j = function
+      | i when i = n -> 0
+      | i ->
+          let a = dmap (get_uint8 cs i)
+          and b = dmap (get_uint8 cs (i + 1))
+          and (d, pad) =
+            let x = get_uint8 cs (i + 3) in try (dmap x, 0) with
+              | Not_found when x = padding -> (0, 1) in
+          let (c, pad) =
+            let x = get_uint8 cs (i + 2) in try (dmap x, pad) with
+              | Not_found when x = padding && pad = 1 -> (0, 2) in
+  
+          emit a b c d j ;
+          match pad with
+          | 0 -> dec (j + 3) (i + 4)
+          | _ when i + 4 <> n -> raise Not_found
+          | _ -> pad
+    in
+    try let pad = dec 0 0 in Some (sub cs' 0 (n' - pad))
+    with Invalid_argument _ | Not_found -> None
+  
+  
+  let is_base64_char c =
+    try ( ignore @@ dmap (int_of_char c) ; true )
+    with Not_found -> false
+end
+
 module Old_version = struct
   let default_alphabet =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
@@ -92,6 +182,13 @@ let b64_encode_and_decode len =
   let _decoded = Base64.decode_exn encoded in
   ()
 
+let b64c_encode_and_decode len =
+  let input = random len in
+  Staged.stage @@ fun () ->
+  let encoded = Cstruct.of_string (Base64.encode_exn input) in
+  let _decoded = Base64.decode_exn (Cstruct.to_string encoded) in
+  ()
+
 let old_encode_and_decode len =
   let input = random len in
   Staged.stage @@ fun () ->
@@ -99,17 +196,33 @@ let old_encode_and_decode len =
   let _decoded = Old_version.decode encoded in
   ()
 
+let mirage_crypto_encode_and_decode len =
+  let input = random len in
+  let input = Cstruct.of_string input in
+  Staged.stage @@ fun () ->
+  let encoded = Crypto_version.encode input in
+  let _decoded = Crypto_version.decode encoded in
+  ()
+
 let args = [ 0; 10; 50; 100; 500; 1000; 2500; 5000 ]
 
 let test_b64 =
-  Test.create_indexed ~name:"Base64"
+  Bench.Test.create_indexed ~name:"Base64"
     ~args b64_encode_and_decode
 
 let test_old =
-  Test.create_indexed ~name:"Old"
+  Bench.Test.create_indexed ~name:"Nocrypto"
     ~args old_encode_and_decode
 
+let test_cur =
+  Bench.Test.create_indexed ~name:"Crypto"
+    ~args mirage_crypto_encode_and_decode
+
+let test_b64c =
+  Bench.Test.create_indexed ~name:"Base64 (cstruct)"
+    ~args b64c_encode_and_decode
+
 let command =
-  Bench.make_command [ test_b64; test_old ]
+  Bench.make_command [ test_b64; test_old; test_cur; test_b64c ]
 
 let () = Command.run command
